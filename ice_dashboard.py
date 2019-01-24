1fe29c0e24d556ea8cc0cf606e73ad9d04208407
James <tytechortz@gmail.com>
2019-01-24 12:13:04 -0700

    connects db and displays data

--- ice_dashboard.py
+++ ice_dashboard.py
@@ -1,24 +1,18 @@
-#######
-# Objective: build a dashboard that imports OldFaithful.csv
-# from the data directory, and displays a scatterplot.
-# The field names are:
-# 'D' = date of recordings in month (in August),
-# 'X' = duration of the current eruption in minutes (to nearest 0.1 minute),
-# 'Y' = waiting time until the next eruption in minutes (to nearest minute).
-######
-
-# Perform imports here:
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
+import sqlite3
+
+# database connection
+cnx = sqlite3.connect('sea-ice.db')
 
 # Launch the application:
 app = dash.Dash()
 
 # Create a DataFrame from the .csv file:
-df = pd.read_csv('../data/OldFaithful.csv')
+df = pd.read_sql_query("SELECT * FROM ice", cnx)
 
 # Create a Dash layout that contains a Graph component:
 app.layout = html.Div([
@@ -27,15 +21,15 @@ app.layout = html.Div([
         figure={
             'data': [
                 go.Scatter(
-                    x = df['X'],
-                    y = df['Y'],
+                    x = df["date"],
+                    y = df["2010's Average"],
                     mode = 'markers'
                 )
             ],
             'layout': go.Layout(
-                title = 'Old Faithful Eruption Intervals v Durations',
-                xaxis = {'title': 'Duration of eruption (minutes)'},
-                yaxis = {'title': 'Interval to next eruption (minutes)'},
+                title = 'Arctic Sea Ice Extent',
+                xaxis = {'title': 'Day'},
+                yaxis = {'title': 'Ice extent (km2)'},
                 hovermode='closest'
             )
         }
