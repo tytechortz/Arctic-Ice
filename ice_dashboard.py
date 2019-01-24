a8b905b1e97a8edb46e58c7d9852fa48026e0bbb
James <tytechortz@gmail.com>
2019-01-24 14:07:36 -0700

    adds dropdown for data selection by column

--- ice_dashboard.py
+++ ice_dashboard.py
@@ -14,26 +14,38 @@ app = dash.Dash()
 # Create a DataFrame from the .csv file:
 df = pd.read_sql_query("SELECT * FROM ice", cnx)
 
+# year_options = []
+# for column in df
+
 # Create a Dash layout that contains a Graph component:
 app.layout = html.Div([
-    dcc.Graph(
-        id='old_faithful',
-        figure={
-            'data': [
-                go.Scatter(
-                    x = df["date"],
-                    y = df["2010's Average"],
-                    mode = 'markers'
+
+    html.Div([
+        dcc.Graph(
+            id='ice_extent',
+            figure={
+                'data': [
+                    go.Scatter(
+                        x = df["#num"],
+                        y = df["2010's Average"],
+                        mode = 'markers'
+                    )
+                ],
+                'layout': go.Layout(
+                    title = 'Arctic Sea Ice Extent',
+                    xaxis = {'title': 'Day'},
+                    yaxis = {'title': 'Ice extent (km2)'},
+                    hovermode='closest'
                 )
-            ],
-            'layout': go.Layout(
-                title = 'Arctic Sea Ice Extent',
-                xaxis = {'title': 'Day'},
-                yaxis = {'title': 'Ice extent (km2)'},
-                hovermode='closest'
-            )
-        }
-    )
+            }
+        ),
+        dcc.Dropdown(
+            id='xaxis',
+            options=[{'label': i, 'value': i} for i in df.columns],
+            value='Loc',
+            placeholder="Select Data"
+        )
+    ])
 ])
 
 # Add the server clause:
