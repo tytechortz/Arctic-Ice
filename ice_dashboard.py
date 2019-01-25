04b185f315ec330ce5c405a05dae2d7df02d3a56
James <tytechortz@gmail.com>
2019-01-25 06:17:16 -0700

    graphs 1 year of data

--- ice_dashboard.py
+++ ice_dashboard.py
@@ -18,17 +18,36 @@ df = pd.read_sql_query("SELECT * FROM ice", cnx)
 years = df.columns 
 
 app.layout = html.Div([
+    dcc.Graph(id='ice-extent'),
+
     html.Div([
         dcc.Dropdown(
             id='xaxis',
             options=[{'label': i, 'value': i} for i in years],
-            value='displacement'
+            # value='#num'
         )
-    ],
-    
-    )
+    ])
 ])
 
+@app.callback(
+    Output('ice-extent', 'figure'),
+    [Input('xaxis', 'value')])
+def update_graph(xaxis_name):
+    return {
+        'data': [go.Scatter(
+            x=df['#num'],
+            y=df[xaxis_name],
+            mode='markers',
+        )],
+        'layout': go.Layout(
+                title = 'Arctic Sea Ice Extent',
+                xaxis = {'title': 'Day'},
+                yaxis = {'title': 'Ice extent (km2)'},
+                hovermode='closest'
+            )
+    }
+
+    
 # Add the server clause:
 if __name__ == '__main__':
     app.run_server()
