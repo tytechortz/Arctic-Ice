97fe1791ee84514ca08fef9fab589d22099a140b
James <tytechortz@gmail.com>
2019-01-25 04:53:10 -0700

    starts refactor

--- ice_dashboard.py
+++ ice_dashboard.py
@@ -4,6 +4,7 @@ import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
 import sqlite3
+from dash.dependencies import Input, Output
 
 # database connection
 cnx = sqlite3.connect('sea-ice.db')
@@ -14,38 +15,18 @@ app = dash.Dash()
 # Create a DataFrame from the .csv file:
 df = pd.read_sql_query("SELECT * FROM ice", cnx)
 
-# year_options = []
-# for column in df
+years = df.columns 
 
-# Create a Dash layout that contains a Graph component:
 app.layout = html.Div([
-
     html.Div([
-        dcc.Graph(
-            id='ice_extent',
-            figure={
-                'data': [
-                    go.Scatter(
-                        x = df["#num"],
-                        y = df["2010's Average"],
-                        mode = 'markers'
-                    )
-                ],
-                'layout': go.Layout(
-                    title = 'Arctic Sea Ice Extent',
-                    xaxis = {'title': 'Day'},
-                    yaxis = {'title': 'Ice extent (km2)'},
-                    hovermode='closest'
-                )
-            }
-        ),
         dcc.Dropdown(
             id='xaxis',
-            options=[{'label': i, 'value': i} for i in df.columns],
-            value='Loc',
-            placeholder="Select Data"
+            options=[{'label': i, 'value': i} for i in years],
+            value='displacement'
         )
-    ])
+    ],
+    
+    )
 ])
 
 # Add the server clause:
