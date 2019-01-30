4e941b9b6009ca85b3154a12e041541126c151f9
James <tytechortz@gmail.com>
2019-01-29 17:48:39 -0700

    hard coded year workig

--- practice.py
+++ practice.py
@@ -5,6 +5,7 @@ import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
 import sqlite3
+from dash.dependencies import Input, Output
 
 # Launch the application:
 app = dash.Dash()
@@ -16,28 +17,58 @@ df = pd.read_sql_query("SELECT * FROM ice", cnx)
 years = df.columns
 
 
-
-# Create a Dash layout that contains a Graph component:
 app.layout = html.Div([
-    dcc.Graph(
-        id='ice',
-        figure={
-            'data': [
-                go.Scatter(
-                    x = df['#num'],
-                    y = df['2000'],
-                    mode = 'markers'
-                )
-            ],
-            'layout': go.Layout(
+    dcc.Graph(id='ice-extent'),
+
+    html.Div([
+        dcc.Dropdown(
+            id='xaxis',
+            options=[{'label': i, 'value': i} for i in years],
+            placeholder='select years'
+        )
+    ])
+])
+
+
+@app.callback(
+    Output('ice-extent', 'figure'),
+    [Input('xaxis', 'value')])
+def update_graph(xaxis_name):
+    return{
+        'data': [
+            go.Scatter(
+                x = df['#num'],
+                y = df['2000'],
+                mode='markers',
+        )],
+        'layout': go.Layout(
                 title = 'Arctic Sea Ice Extent',
                 xaxis = {'title': 'Day'},
                 yaxis = {'title': 'Ice Extent in km2'},
                 hovermode='closest'
             )
-        }
-    )
-])
+    }
+
+# app.layout = html.Div([
+#     dcc.Graph(
+#         id='ice',
+#         figure={
+#             'data': [
+#                 go.Scatter(
+#                     x = df['#num'],
+#                     y = df['2000'],
+#                     mode = 'markers'
+#                 )
+#             ],
+#             'layout': go.Layout(
+#                 title = 'Arctic Sea Ice Extent',
+#                 xaxis = {'title': 'Day'},
+#                 yaxis = {'title': 'Ice Extent in km2'},
+#                 hovermode='closest'
+#             )
+#         }
+#     )
+# ])
 
 # Add the server clause:
 if __name__ == '__main__':
