68bf174255fe1ec9c95eb52ea3e19f7f8644948f
James <tytechortz@gmail.com>
2019-01-30 13:18:13 -0700

    index dropdown

--- practice.py
+++ practice.py
@@ -1,4 +1,3 @@
-# Perform imports here:
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -7,15 +6,16 @@ import pandas as pd
 import sqlite3
 from dash.dependencies import Input, Output
 
+# database connection
+cnx = sqlite3.connect('sea-ice.db')
+
 # Launch the application:
 app = dash.Dash()
 
-cnx = sqlite3.connect('sea-ice.db')
-
+# Create a DataFrame from the .csv file:
 df = pd.read_sql_query("SELECT * FROM ice", cnx)
 
-years = df.columns
-
+years = df.columns[4:]
 
 app.layout = html.Div([
     dcc.Graph(id='ice-extent'),
@@ -24,34 +24,31 @@ app.layout = html.Div([
         dcc.Dropdown(
             id='xaxis',
             options=[{'label': i, 'value': i} for i in years],
-            placeholder='select years',
-            multi=True
+            placeholder='select years'
+            # value='#num'
         )
     ])
 ])
 
-
 @app.callback(
     Output('ice-extent', 'figure'),
     [Input('xaxis', 'value')])
-def update_graph(selected_year):
-    df_trans = df.T
-    print(df_trans)
-    # return{
-    #     'data': [
-    #         go.Scatter(
-    #             x = df.transpose(),
-    #             y = df['num'],
-    #             mode='lines',
-    #     )],
-    #     'layout': go.Layout(
-    #             title = 'Arctic Sea Ice Extent',
-    #             xaxis = {'title': 'Day'},
-    #             yaxis = {'title': 'Ice Extent in km2'},
-    #             hovermode='closest'
-    #         )
-    # }
+def update_graph(xaxis_name):
+    return {
+        'data': [go.Scatter(
+            x=df['#num'],
+            y=df[xaxis_name],
+            mode='lines',
+        )],
+        'layout': go.Layout(
+                title = 'Arctic Sea Ice Extent',
+                xaxis = {'title': 'Day'},
+                yaxis = {'title': 'Ice extent (km2)'},
+                hovermode='closest'
+            )
+    }
 
+    
 # Add the server clause:
 if __name__ == '__main__':
     app.run_server()
