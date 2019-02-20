1e62282910e2149079375b8a1e5d4cfad869ce06
James <tytechortz@gmail.com>
2019-02-20 15:03:00 -0700

    adds rankings fr annual min and max

--- practice.py
+++ practice.py
@@ -46,14 +46,17 @@ record_max = df[' (0) Northern_Hemisphere'].max(),
 record_max_difference = today_value - record_max[0]
 
 
+
 df2=pd.read_csv('ftp://sidads.colorado.edu/DATASETS/NOAA/G02186/masie_4km_allyears_extent_sqkm.csv', skiprows=1)
 df2['yyyyddd'] = pd.to_datetime(df2['yyyyddd'], format='%Y%j')
 df2.set_index('yyyyddd', inplace=True)
-df3 = df2[' (0) Northern_Hemisphere'].loc[df2.groupby(pd.Grouper(freq='Y')).idxmax().iloc[:-1, 0]]
+df3 = df2[' (0) Northern_Hemisphere'].loc[df2.groupby(pd.Grouper(freq='Y')).idxmax().iloc[:, 0]]
 df4 = df3.sort_values(axis=0, ascending=False)
-
+df_min = df2[' (0) Northern_Hemisphere'].loc[df2.groupby(pd.Grouper(freq='Y')).idxmin().iloc[:, 0]]
+df5 = df_min.sort_values(axis=0, ascending=False)
 
 df_min_max = df3[0]
+record_low_max_difference = today_value - df_min_max
 
 
 annual_maximums = df2[' (0) Northern_Hemisphere'].loc[df2.groupby(pd.Grouper(freq='Y')).idxmax().iloc[:-1, 0]]
@@ -143,17 +146,7 @@ body = html.Div([
                     html.H2("Today's Value: {:,.1f} km2".format(today_value)),
                 ]),
                 style={'height':40, 'align':'start'} 
-            ), 
-        ]),
-        dbc.Row([
-            dbc.Col(
-                html.Div([
-                     html.H2("24 Hour Change: {:,.1f} km2".format(daily_difference)),
-                ]),
-                style={'height':40, 'align':'end'} 
-            ), 
-        ]),
-        dbc.Row([
+            ),
             dbc.Col(
                 html.Div([
                     html.H2('Record Minimum: {:,.1f} km2'.format(record_min[0])),
@@ -164,7 +157,13 @@ body = html.Div([
         dbc.Row([
             dbc.Col(
                 html.Div([
-                    html.H2('Difference From Minimum: {:,.1f} km2'.format(record_min_difference)),
+                     html.H2("24 Hour Change: {:,.1f} km2".format(daily_difference)),
+                ]),
+                style={'height':40, 'align':'end'} 
+            ),
+            dbc.Col(
+                html.Div([
+                    html.H2('Difference: {:,.1f} km2'.format(record_min_difference)),
                 ]),
                 style={'height':40, 'align':'start'} 
             ), 
@@ -175,39 +174,172 @@ body = html.Div([
                     html.H2('Record Maximum: {:,.1f} km2'.format(record_max[0])),
                 ]),
                 style={'height':40, 'align':'end'} 
-            ), 
-        ]),
-        dbc.Row([
-            dbc.Col(
-                html.Div([
-                    html.H2('Difference From Maximum: {:,.1f} km2'.format(record_max_difference)),
-                ]),
-                style={'height':40, 'align':'start'} 
-            ), 
-        ]),
-        dbc.Row([
+            ),
             dbc.Col(
                 html.Div([
                     html.H2('Record Low Maximum: {:,.1f} km2'.format(df_min_max)),
                 ]),
                 style={'height':40, 'align':'start'} 
-            ), 
+            ),  
         ]),
         dbc.Row([
             dbc.Col(
                 html.Div([
-                    html.H2('Annual Maximum Rankings'),
+                    html.H2('Difference: {:,.1f} km2'.format(record_max_difference)),
                 ]),
                 style={'height':40, 'align':'start'} 
             ),
             dbc.Col(
                 html.Div([
-                    html.H2("1- {:,.1f} km2".format(df4[12])),
+                    html.H2('Difference: {:,.1f} km2'.format(record_low_max_difference)),
                 ]),
                 style={'height':40, 'align':'start'} 
             ), 
         ]),
-        
+        dbc.Row([
+            dbc.Col(
+                html.Div([
+                    html.H2('Annual Low Maximum Rankings',style={'color': 'black','font-size':40}),
+                ]),
+                style={'height':40, 'align':'start'} 
+            ),
+            dbc.Col(
+                html.Div([
+                    html.H2('Annual Minimum Rankings',style={'color': 'black','font-size':40}),
+                ]),
+                style={'height':40, 'align':'start'} 
+            ),
+        ]),
+        dbc.Row([
+            dbc.Col(
+                html.Div([
+                    html.H2("1- {:,.1f} km2".format(df4[13]))
+                ])
+            ),
+            dbc.Col(
+                html.Div([
+                    html.H2("1- {:,.1f} km2".format(df5[13]))
+                ])
+            )
+        ]),
+
+        dbc.Row([
+            dbc.Col(
+                html.Div([
+                    html.H2("2- {:,.1f} km2".format(df4[12])),
+                ]),
+                style={'height':40, 'align':'start'} 
+            ),
+            dbc.Col(
+                html.Div([
+                    html.H2("2- {:,.1f} km2".format(df5[12]))
+                ])
+            ) 
+        ]),
+        dbc.Row([
+            dbc.Col(
+                html.Div([
+                    html.H2("3- {:,.1f} km2".format(df4[11])),
+                ]),
+                style={'height':40, 'align':'start'} 
+            ),
+            dbc.Col(
+                html.Div([
+                    html.H2("3- {:,.1f} km2".format(df5[11]))
+                ])
+            ) 
+        ]),
+        dbc.Row([
+            dbc.Col(
+                html.Div([
+                    html.H2("4- {:,.1f} km2".format(df4[10])),
+                ]),
+                style={'height':40, 'align':'start'} 
+            ),
+            dbc.Col(
+                html.Div([
+                    html.H2("4- {:,.1f} km2".format(df5[10]))
+                ])
+            ) 
+        ]),
+        dbc.Row([
+            dbc.Col(
+                html.Div([
+                    html.H2("5- {:,.1f} km2".format(df4[9])),
+                ]),
+                style={'height':40, 'align':'start'} 
+            ),
+            dbc.Col(
+                html.Div([
+                    html.H2("5- {:,.1f} km2".format(df5[9]))
+                ])
+            ) 
+        ]),
+        dbc.Row([
+            dbc.Col(
+                html.Div([
+                    html.H2("6- {:,.1f} km2".format(df4[8])),
+                ]),
+                style={'height':40, 'align':'start'} 
+            ),
+            dbc.Col(
+                html.Div([
+                    html.H2("6- {:,.1f} km2".format(df5[8]))
+                ])
+            ) 
+        ]),
+        dbc.Row([
+            dbc.Col(
+                html.Div([
+                    html.H2("7- {:,.1f} km2".format(df4[7])),
+                ]),
+                style={'height':40, 'align':'start'} 
+            ),
+            dbc.Col(
+                html.Div([
+                    html.H2("7- {:,.1f} km2".format(df5[7]))
+                ])
+            ) 
+        ]),
+        dbc.Row([
+            dbc.Col(
+                html.Div([
+                    html.H2("8- {:,.1f} km2".format(df4[6])),
+                ]),
+                style={'height':40, 'align':'start'} 
+            ),
+            dbc.Col(
+                html.Div([
+                    html.H2("8- {:,.1f} km2".format(df5[6]))
+                ])
+            ) 
+        ]),
+        dbc.Row([
+            dbc.Col(
+                html.Div([
+                    html.H2("9- {:,.1f} km2".format(df4[5])),
+                ]),
+                style={'height':40, 'align':'start'} 
+            ),
+            dbc.Col(
+                html.Div([
+                    html.H2("9- {:,.1f} km2".format(df5[5]))
+                ])
+            ) 
+        ]),
+        dbc.Row([
+            dbc.Col(
+                html.Div([
+                    html.H2("10- {:,.1f} km2".format(df4[4])),
+                ]),
+                style={'height':40, 'align':'start'} 
+            ),
+            dbc.Col(
+                html.Div([
+                    html.H2("10- {:,.1f} km2".format(df5[4]))
+                ])
+            ) 
+        ]),
     ]),
 ])
 
