1f6631eda4cad07b6eecf14734eae8363d507102
James <tytechortz@gmail.com>
2019-03-05 08:49:13 -0700

    adds dropdown year selector

--- practice.py
+++ practice.py
@@ -19,11 +19,20 @@ app.config['suppress_callback_exceptions']=True
 
 pd.options.display.float_format = '{:,}'.format
 
+value_range = [0, 365]
+
+
+
 # Read data
 df = pd.read_csv('ftp://sidads.colorado.edu/DATASETS/NOAA/G02186/masie_4km_allyears_extent_sqkm.csv', skiprows=1)
 # Format date and set indext to date
 df['yyyyddd'] = pd.to_datetime(df['yyyyddd'], format='%Y%j')
 df.set_index('yyyyddd', inplace=True)
+
+# Dropdown year selector values
+year_options = []
+for YEAR in df.index.year.unique():
+    year_options.append({'label':(YEAR), 'value':YEAR})
 
 # Change dataframe to 5 day trailing average
 df_fdta = df.rolling(window=5).mean()
@@ -123,10 +132,79 @@ body = html.Div([
                 width={'size': 6, 'offset': 3}
             ),
         ]),
+        dbc.Row(
+            [
+            dbc.Col(
+                html.Div(
+                    dcc.Graph(id='ice-extent', style={'height':450}),    
+                ),
+                width={'size':10}
+                ),
+            ],
+        justify='around'
+        ),
+        dbc.Row([
+            dbc.Col(
+                html.Div(
+                    html.H6('Select Years', style={'text-align': 'center', 'color': 'black'}),
+                ),
+                width={'size': 6, 'offset': 3}
+            ),
+        ]),
+        dbc.Row(
+            [
+                dbc.Col(
+                    dcc.Dropdown(id='year1',options=year_options
+                    ), 
+                    width={'size':2, 'offset':2},
+                ),
+                dbc.Col(
+                    dcc.Dropdown(id='year2',options=year_options
+                    ),
+                    width={'size':2} 
+                ),
+                dbc.Col(
+                    dcc.Dropdown(id='year3',options=year_options
+                    ),
+                    width={'size':2} 
+                ),
+                dbc.Col(
+                    dcc.Dropdown(id='year4',options=year_options
+                    ),
+                    width={'size':2} 
+                ),
+            ],
+            style={'height':20}
+        ),
     ])
 ])
 
-
+@app.callback(
+    Output('ice-extent', 'figure'),
+    [Input('year1', 'value'),
+    Input('year2', 'value'),
+    Input('year3', 'value'),
+    Input('year4', 'value'),])
+def update_figure(selected_year1,selected_year2, selected_year3, selected_year4):
+    traces = []
+    selected_years = [selected_year1,selected_year2,selected_year3,selected_year4]
+    for year in selected_years:
+        df5=df[df.index.year == year]
+        traces.append(go.Scatter(
+            # x=df2['yyyyddd'],
+            y=df5[' (0) Northern_Hemisphere'],
+            mode='lines',
+            name=year
+        ))
+    return {
+        'data': traces,
+        'layout': go.Layout(
+                title = 'Arctic Sea Ice Extent',
+                xaxis = {'title': 'Day', 'range': value_range},
+                yaxis = {'title': 'Ice extent (km2)'},
+                hovermode='closest',
+                )  
+    }
 
 app.layout = html.Div(body)
 
