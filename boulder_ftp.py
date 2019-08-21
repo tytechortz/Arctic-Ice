f85de61128eccc36804f2bc1b89cc8363a58c41b
James Swank <jmswank7@gmail.com>
2019-08-20 21:15:00 -0600

    downloads data with env variable pswd

--- boulder_ftp.py
+++ boulder_ftp.py
@@ -1,10 +1,15 @@
 from ftplib import FTP
+import os
 
 ftp = FTP('sidads.colorado.edu')
 # ftp = FTP('ftp.cse.buffalo.edu')
-ftp.login(user='anonymous', passwd='')
+ftp.login(user='anonymous', passwd='ICE_PSWD')
 # ftp.login()
 
 print(ftp.login())
 
+ftp.cwd('/pub/DATASETS/NOAA/G02135/north/daily/data/')
+files = ftp.dir()
+ftp.retrbinary('RETR N_seaice_extent_daily_v3.0.csv', open('N_seaice_extent_daily_v3.0.csv', 'wb').write)
+ftp.quit()
 
